7751169459d18683b6aea61dbc5615defdb62cf9
samuelgbenga <samuelgbenga972@gmail.com>
2025-04-12 05:40:21 +0100

    first commit for my fx_simulator_monolitic

---
+++ src/main/java/ng/samuel/fxmono/simulator/FxSimulatorApp.java
@@ -0,0 +1,13 @@
+package ng.samuel.fxmono.simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxSimulatorApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxSimulatorApp.class, args);
+	}
+
+}
---
+++ src/test/java/ng/samuel/fxmono/simulator/FxSimulatorAppTests.java
@@ -0,0 +1,13 @@
+package ng.samuel.fxmono.simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxSimulatorAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
